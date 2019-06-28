10e652e37471bbcda6fd72d21ae28a6f7b962148
Mariano <marianorv97@gmail.com>
2019-06-28 01:40:56 -0600

    Se agregaron más lugares

--- pages/lugares.vue
+++ pages/lugares.vue
@@ -16,37 +16,17 @@
       <!-- LUGAR TURISTICO 1  -->
       <div id="lugar1" class="carousel slide col-12 col-md-6" data-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item active bg-primary">
-            <img
-              class="d-block w-100"
-              src="../assets/imagenes-lugares/VolcanSantaMaria1.jpg"
-              height="300px"
-              alt="First slide"
-            >
+          <div class="carousel-item active ">
+            <img class="d-block w-100" src="../assets/imagenes-lugares/VolcanSantaMaria1.jpg" height="300px" alt="First slide">
           </div>
-          <div class="carousel-item bg-success">
-            <img
-              class="d-block w-100"
-              src="../assets/imagenes-lugares/VolcanSantaMaria2.jpg"
-              height="300px"
-              alt="Second slide"
-            >
+          <div class="carousel-item ">
+            <img class="d-block w-100" src="../assets/imagenes-lugares/VolcanSantaMaria2.jpg" height="300px" alt="Second slide">
           </div>
-          <div class="carousel-item bg-danger">
-            <img
-              class="d-block w-100"
-              src="../assets/imagenes-lugares/VolcanSantaMaria3.jpg"
-              height="300px"
-              alt="Third slide"
-            >
+          <div class="carousel-item ">
+            <img class="d-block w-100" src="../assets/imagenes-lugares/VolcanSantaMaria3.jpg" height="300px" alt="Third slide">
           </div>
-          <div class="carousel-item bg-danger">
-            <img
-              class="d-block w-100"
-              src="../assets/imagenes-lugares/VolcanSantaMaria4.png"
-              height="300px"
-              alt="Fourth slide"
-            >
+          <div class="carousel-item ">
+            <img class="d-block w-100" src="../assets/imagenes-lugares/VolcanSantaMaria4.png" height="300px" alt="Fourth slide">
           </div>
         </div>
         <a class="carousel-control-prev" href="#lugar1" role="button" data-slide="prev">
@@ -59,6 +39,123 @@
         </a>
       </div>
     </div>
+    <br><br>
+    <div class="row">
+      <div id="lugar2" class="carousel slide col-12 col-md-6" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active ">
+            <img class="d-block w-100" src="../assets/imagenes-lugares/ParqueCentral1.jpg" height="300px" alt="First slide">
+          </div>
+          <div class="carousel-item ">
+            <img class="d-block w-100" src="../assets/imagenes-lugares/ParqueCentral2.jpg" height="300px" alt="Second slide">
+          </div>
+          <div class="carousel-item ">
+            <img class="d-block w-100" src="../assets/imagenes-lugares/ParqueCentral3.jpg" height="300px" alt="Third slide">
+          </div>
+          <div class="carousel-item ">
+            <img class="d-block w-100" src="../assets/imagenes-lugares/ParqueCentral4.jpg" height="300px" alt="Fourth slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#lugar2" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#lugar2" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <div class="col-12 col-md-6 ">
+        <h2 class="text-center">Parque a Centroamérica</h2>
+        <p Align="justify">Consistía en realidad de 2 jardines, uno de los cuales era parte o extensión del atrio de la iglesia. Estaban separados por un portal cuya función era plenamente comercial. En 1938 se trazó su distribución final, durante el gobierno de Jorge Ubico. Los antiguos jardines fueron integrados en un solo cuerpo. Fue y sigue siendo centro de actividades importantes de caracter social y político, sufrió varias remodelaciones. El kiosko del Parque es un monumento a uno de los autores del Himno Nacional: Rafael Álfavez Ovalle.</p>
+      </div>
+    </div>
+    <br><br>
+    <div class="row">
+      <div class="col-12 col-md-6 ">
+        <h2 class="text-center">Cerro El Baúl</h2>
+        <p Align="justify">Constituye el pulmón urbano de la ciudad, el cual ha sido tradicionalmente un centro de recreación familiar. Puede subirse desde el departamento de Quetzaltenango ya sea a pie, en carro o bicicleta. Podrá observarse el casco urbano rodeado de pura naturaleza.
+        </p>
+      </div>
+      <div id="lugar3" class="carousel slide col-12 col-md-6" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active ">
+            <img class="d-block w-100" src="../assets/imagenes-lugares/ElBaul1.jpg" height="300px" alt="First slide">
+          </div>
+          <div class="carousel-item ">
+            <img class="d-block w-100" src="../assets/imagenes-lugares/ElBaul2.jpg" height="300px" alt="Second slide">
+          </div>
+          <div class="carousel-item ">
+            <img class="d-block w-100" src="../assets/imagenes-lugares/ElBaul3.jpg" height="300px" alt="Third slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#lugar3" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#lugar3" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+    <br><br>
+    <div class="row">
+      <div id="lugar4" class="carousel slide col-12 col-md-6" data-ride="carousel">
+        <div class="carousel-inner">
+        <div class="carousel-item active ">
+        <img class="d-block w-100" src="../assets/imagenes-lugares/Teatro1.jpg" height="300px" alt="First slide">
+        </div>
+        <div class="carousel-item ">
+        <img class="d-block w-100" src="../assets/imagenes-lugares/Teatro2.jpg" height="300px" alt="Second slide">
+        </div>
+        <div class="carousel-item ">
+        <img class="d-block w-100" src="../assets/imagenes-lugares/Teatro3.jpg" height="300px" alt="Third slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#lugar4" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#lugar4" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+      </div>
+      <div class="col-12 col-md-6 ">
+        <h2 class="text-center">Teatro Municipal</h2>
+        <p Align="justify">Este teatro pasó por varias restauraciones. Su diseño original es obra del arquitecto Durini, quien logra un estilo del neorrenacimiento francés debido a que hizo uso de elementos decorativos típicos de la cultura francesa. El entorno del teatro ha sido adornado con varios monumentos a lo largo del tiempo, de ellos el que más se destaca es la estatua de Artemisa, personaje de la mitología griega, diosa de la naturaleza, las cosechas y la caza.</p>
+      </div>
+    </div>
+    <br><br>
+    <div class="row">
+      <div class="col-12 col-md-6 ">
+        <h2 class="text-center">Catedral del Espíritu Santo</h2>
+        <p Align="justify">Se empezó a construir aproximadamente en mayo de 1532. Este templo fue fundado por los españoles luego de conquistar la Ciudad de Quetzaltenango y en honor al Espíritu Santo. Luego de sufrir deterioros por los terremotos de 1765, 1853 y otros años, la nueva catedral se construyó y el resultado fue la impresión de que hay dos iglesias unidas, pero en realidad es la fachada de la catedral antigua y la nueva construcción.</p>
+      </div>
+      <div id="lugar5" class="carousel slide col-12 col-md-6" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active ">
+          <img class="d-block w-100" src="../assets/imagenes-lugares/Catedral1.jpg" height="300px" alt="First slide">
+          </div>
+          <div class="carousel-item ">
+          <img class="d-block w-100" src="../assets/imagenes-lugares/Catedral2.jpg" height="300px" alt="Second slide">
+          </div>
+          <div class="carousel-item ">
+          <img class="d-block w-100" src="../assets/imagenes-lugares/Catedral3.jpg" height="300px" alt="Third slide">
+          </div>
+        </div>
+      <a class="carousel-control-prev" href="#lugar5" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#lugar5" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+      </div>
+    </div>
+    <hr>
   </div>
 </template>
 
