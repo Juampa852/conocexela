54a272bd8678af4ad4900b682ccab230dfbba67c
Juan Miguel de León <juanmidlc@gmail.com>
2019-06-28 00:53:06 -0600

    New index

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,69 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        ConoceXela
-      </h1>
-      <h2 class="subtitle">
-        Webpage de ConoceXela
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<body>
+   <main>
+       <div style="width:1338px; height:180px;float:none;">
+           <figure>
+                <img style="float:left; margin:10px;" src="https://aprende.guatemala.com/wp-content/uploads/2017/08/Xela.jpg" width="250px" height="150px"/>
+           </figure>
+           <p ALIGN="left">Quetzaltenango, es una ciudad guatemalteca ubicada en el departamento del mismo nombre, también llamada popularmente "Xelajú" o simplemente "Xela" (shela).
+              Así mismo, es la segunda ciudad más importante de Guatemala, a 206 km al noroeste de la Ciudad de Guatemala. La ciudad se encuentra ubicada en un valle montañoso
+              en el altiplano occidental de Guatemala con una altitud media sobre el nivel del mar de 2333 msnm. Cuenta con una población de 800 000 habitantes.
+              Quetzaltenango cuenta con diversos centros educativos, como algunas de las universidades más reconocidas del país. Es la ciudad con la mayor cantidad de centros educativos
+              por habitante por su ubicación estratégica, ya que la mayoría de sus estudiantes no son oriundos de la ciudad, sino de pueblos cercanos.
+            </p>
+       </div>
+       <div style="width:1338px; height:180px;float:none;">
+            <figure>
+                 <img style="float:right; margin:10px;" src="https://aprende.guatemala.com/wp-content/uploads/2017/08/Xela.jpg" width="250px" height="150px"/>
+            </figure>
+            <p ALIGN="right">Quetzaltenango fue proclamada como Capital de Centroamérica por el Parlamento Centroamericano en 2008. Es la cabecera del departamento de Quetzaltenango, que tiene 1953 km²,
+                equivalentes al 1,8 % del territorio nacional. Quetzaltenango limita al norte con el departamento de Huehuetenango, al este con los departamentos de Totonicapán y Sololá, al sur con los departamentos
+                de Retalhuleu y Suchitepéquez y al oeste con el departamento de San Marcos. Sus principales idiomas son el castellano, mam y k’iche’. Su temperatura habitual es de frío a templado y su fiesta titular es
+                del 9 al 17 de septiembre, para celebrar la independencia de la patria.
+            </p>
+        </div>
+        <div style="width:1338px; height:180px;float:none;">
+            <figure>
+                <img style="float:left; margin:10px;" src="https://aprende.guatemala.com/wp-content/uploads/2017/08/Xela.jpg" width="250px" height="150px"/>
+            </figure>
+            <p ALIGN="left">Después de libra una dura batalla los españoles en el área sur de Guatemala hoy en día, Pedro de Alvarado se dirigió río arriba internándose en la Sierra Madre y llego al hacia el valle fértil
+                de Quetzaltenango, en el centro del reino k'iche'. La caballería logró dispersar a las tropas indígenas y los españoles se acercaron a la ciudad de Xelajú, El príncipe quiché Tecún Umán murió en la batalla
+                cerca de Olintepeque al norte de Quetzaltenango, los relatos de los españoles dejan claro que al menos uno y posiblemente dos de los señores de Q'umarkaj fallecieron en los intensos combates durante el
+                acercamiento inicial a Quetzaltenango.Sin embargo, según la noción común. Se fundó entonces en 15 de mayo de 1524.
+            </p>
+        </div>
+        <div style="width:1338px; height:180px;float:none;">
+            <figure>
+                <img style="float:right; margin:10px;" src="https://aprende.guatemala.com/wp-content/uploads/2017/08/Xela.jpg" width="250px" height="150px"/>
+            </figure>
+            <p ALIGN="right">En 1825, los diputados Juan José Flores Estrada, Laureano Nova y Manuel Montúfar y Coronado, solicitaron a la Asamblea Constitucional que se le diera el título de ciudad al pueblo de «Quezaltenango».
+                Esta accedió por medio del decreto No. 63 de fecha 29 de octubre. Debe indicarse que algunos escritores refirieron el nombre posteriormente como «Quetzaltenango», indicando que provenía del lugar en que abundaban
+                los quetzales (Pharomachrus mocinno) basándose en el título de Ixquin Nehaib de 1558.a. Su fundación fue el 16 de septiembre de 1845
+            </p>
+        </div>
+        <div style="width:1338px; height:180px;float:none;">
+            <figure>
+                <img style="float:left; margin:10px;" src="https://aprende.guatemala.com/wp-content/uploads/2017/08/Xela.jpg" width="250px" height="150px"/>
+            </figure>
+            <p ALIGN="left">El 2 de febrero de 1838, en la ciudad de Quetzaltenango, un grupo secesionista liberal fundó el independiente Estado de Los Altos el cual pretendió independizarse de Guatemala. Este estado estaba constituido
+                por los modernos departamentos de Quetzaltenango, Huehuetenango, San Marcos, Totonicapán y Sololá— y era donde se aglutinaban los más importantes criollos miembros del Partido Liberal de Guatemala y los enemigos liberales
+                del régimen conservador.</p>
+        </div>
+        <div style="width:1338px; height:180px;float:none;">
+            <figure>
+                <img style="float:right; margin:10px;" src="https://aprende.guatemala.com/wp-content/uploads/2017/08/Xela.jpg" width="250px" height="150px"/>
+            </figure>
+            <p ALIGN="right">Actualmente la ciudad de Quetzaltenango, es la segunda ciudad en importancia en Guatemala, destacada por la gran cantidad de escuelas, colegios, centros hospitalarios y universidades, siendo hoy día centro
+                cultural del occidente del país. Así mismo en la ciudad hay gran cantidad de escuelas de español, a las que asisten personas de muchos países del mundo, principalmente de México y E.U.A. La educación del español como segundo
+                idioma se imparte con excursiones ecológicas y turismo de aventura. La ciudad también tiene hoy día vida nocturna, concentrada en parte de su centro histórico, en donde se puede degustar de comida y bebida nacional e internacional,
+                en sus diversos restaurantes, bares y centros nocturnos, y en muchos municipios de este departamento se encuentran tiendas artesanales con una gran diversidad de artículos artesanales.</p>
+        </div>
+        <div style="width:1338px; height:180px;float:none;">
+            <figure>
+                <img style="float:left; margin:10px;" src="https://aprende.guatemala.com/wp-content/uploads/2017/08/Xela.jpg" width="250px" height="150px"/>
+            </figure>
+            <p ALIGN="left">La ciudad sigue siendo un polo de atracción de inmigrantes de las zonas rurales del país, así como los inmigrantes extranjeros, mayormente del sur de México y de Centroamérica.</p>
+        </div>
+   </main>
+</body>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
