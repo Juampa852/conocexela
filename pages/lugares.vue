5abb8e9237ea61223823ee713ef67cc145fdebc5
Mariano <marianorv97@gmail.com>
2019-06-28 01:21:05 -0600

    Pagina /lugares
    Se creó la página para lugares turísticos y todas las imagenes a utilizar, es necesario corregir la indentación para colocar el codigo html

---
+++ pages/lugares.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="divlugares" class="container">
+    <div class="text-center">
+      <h1>Lugares Turísticos - Xela</h1>
+    </div>
+    <br><br>
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <h2 class="text-center">
+          Volcán Santa María
+        </h2>
+        <p align="justify">
+          Es un volcán inactivo cerca de la ciudad de Quetzaltenango. Es el cuarto volcán más alto de Guatemala, para subirlo toma aproximadamente cuatro horas y tres para el descenso. A veces, en la época fría (octubre-febrero), se cubre de escarcha o nieve y puede verse desde la Ciudad de Quetzaltenango su cono blanco. El Volcán Santiaguito se ubica al sureste del cono del Volcán Santa María. Se ubica a 11 kilómetros aproximadamente de la Ciudad de Quetzaltenango.
+        </p>
+      </div>
+      <!-- LUGAR TURISTICO 1  -->
+      <div id="lugar1" class="carousel slide col-12 col-md-6" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active bg-primary">
+            <img
+              class="d-block w-100"
+              src="../assets/imagenes-lugares/VolcanSantaMaria1.jpg"
+              height="300px"
+              alt="First slide"
+            >
+          </div>
+          <div class="carousel-item bg-success">
+            <img
+              class="d-block w-100"
+              src="../assets/imagenes-lugares/VolcanSantaMaria2.jpg"
+              height="300px"
+              alt="Second slide"
+            >
+          </div>
+          <div class="carousel-item bg-danger">
+            <img
+              class="d-block w-100"
+              src="../assets/imagenes-lugares/VolcanSantaMaria3.jpg"
+              height="300px"
+              alt="Third slide"
+            >
+          </div>
+          <div class="carousel-item bg-danger">
+            <img
+              class="d-block w-100"
+              src="../assets/imagenes-lugares/VolcanSantaMaria4.png"
+              height="300px"
+              alt="Fourth slide"
+            >
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#lugar1" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#lugar1" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
