335d9ae1caca960accb2a2435beaaf98048ffb62
Mariano <marianorv97@gmail.com>
2019-06-30 18:32:43 -0600

    Pagina Lugares
    Ya están todos los lugares visibles, aún faltan detalles de tamaño de imagen y la paginacion

--- pages/lugares.vue
+++ pages/lugares.vue
@@ -1,166 +1,119 @@
 <template>
   <div id="divlugares" class="container">
     <div class="text-center">
-      <h1>Lugares Turísticos - Xela</h1>
+      <h1>Lugares Turísticos</h1>
     </div>
-    <br><br>
+    <br><hr><br>
+    <!-- CICLO PARA MOSTRAR TODOS LOS LUGARES -->
+    <div v-for="(lugar, index) in lugares" :key="index">
+      <!-- ALINEADO A LA IZQUIERDA -->
+      <div v-if="index % 2 === 1">
+        <div class="row">
+          <!-- TEXTO -->
+          <div class="col-12 col-md-6">
+            <h2 class="text-center">
+              {{ index }}. {{ lugar.nombre }}
+            </h2>
+            <p align="justify">
+              {{ lugar.descripcion }}
+            </p>
+          </div>
+          <!-- SLIDER  -->
+          <div class="col-12 col-md-6">
+            <b-carousel
+              id="carousel-fade"
+              style="text-shadow: 0px 0px 2px #000"
+              fade
+              indicators
+              img-width="300"
+              img-height="300"
+            >
+              <b-carousel-slide
+                v-for="imagen in lugar.imagenes"
+                :key="imagen"
+                :img-src="imagen"
+              />
+            </b-carousel>
+          </div>
+        </div>
+        <br><br>
+      </div>
+      <!-- ALINEADO A LA DERECHA -->
+      <div v-else>
+        <div class="row">
+          <!-- SLIDER  -->
+          <div class="col-12 col-md-6">
+            <b-carousel
+              id="carousel-fade"
+              style="text-shadow: 0px 0px 2px #000"
+              fade
+              indicators
+              img-width="300"
+              img-height="300"
+            >
+              <b-carousel-slide
+                v-for="imagen in lugar.imagenes"
+                :key="imagen"
+                :img-src="imagen"
+              />
+            </b-carousel>
+          </div>
+          <!-- TEXTO -->
+          <div class="col-12 col-md-6">
+            <h2 class="text-center">
+              {{ index }}. {{ lugar.nombre }}
+            </h2>
+            <p align="justify">
+              {{ lugar.descripcion }}
+            </p>
+          </div>
+        </div>
+        <br><br>
+      </div>
+    </div>
     <div class="row">
-      <div class="col-12 col-md-6">
-        <h2 class="text-center">
-          Volcán Santa María
-        </h2>
-        <p align="justify">
-          Es un volcán inactivo cerca de la ciudad de Quetzaltenango. Es el cuarto volcán más alto de Guatemala, para subirlo toma aproximadamente cuatro horas y tres para el descenso. A veces, en la época fría (octubre-febrero), se cubre de escarcha o nieve y puede verse desde la Ciudad de Quetzaltenango su cono blanco. El Volcán Santiaguito se ubica al sureste del cono del Volcán Santa María. Se ubica a 11 kilómetros aproximadamente de la Ciudad de Quetzaltenango.
-        </p>
-      </div>
-      <!-- LUGAR TURISTICO 1  -->
-      <div id="lugar1" class="carousel slide col-12 col-md-6" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active ">
-            <img class="d-block w-100" src="../assets/imagenes-lugares/VolcanSantaMaria1.jpg" height="300px" alt="First slide">
-          </div>
-          <div class="carousel-item ">
-            <img class="d-block w-100" src="../assets/imagenes-lugares/VolcanSantaMaria2.jpg" height="300px" alt="Second slide">
-          </div>
-          <div class="carousel-item ">
-            <img class="d-block w-100" src="../assets/imagenes-lugares/VolcanSantaMaria3.jpg" height="300px" alt="Third slide">
-          </div>
-          <div class="carousel-item ">
-            <img class="d-block w-100" src="../assets/imagenes-lugares/VolcanSantaMaria4.png" height="300px" alt="Fourth slide">
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#lugar1" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#lugar1" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
+      <!-- <b-pagination
+        v-model="paginaActual"
+        Align="fill"
+        :total-rows="rows"
+        :per-page="porPagina"
+        first-text="Primera"
+        prev-text="Anterior"
+        next-text="Siguiente"
+        last-text="Última"
+        aria-controls="mis-lugares"
+        size="lg"
+      /> -->
+      <b-pagination
+        v-model="paginaActual"
+        Align="center"
+        :total-rows="rows"
+        :per-page="porPagina"
+        aria-controls="mis-lugares"
+        size="lg"
+      />
     </div>
-    <br><br>
-    <div class="row">
-      <div id="lugar2" class="carousel slide col-12 col-md-6" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active ">
-            <img class="d-block w-100" src="../assets/imagenes-lugares/ParqueCentral1.jpg" height="300px" alt="First slide">
-          </div>
-          <div class="carousel-item ">
-            <img class="d-block w-100" src="../assets/imagenes-lugares/ParqueCentral2.jpg" height="300px" alt="Second slide">
-          </div>
-          <div class="carousel-item ">
-            <img class="d-block w-100" src="../assets/imagenes-lugares/ParqueCentral3.jpg" height="300px" alt="Third slide">
-          </div>
-          <div class="carousel-item ">
-            <img class="d-block w-100" src="../assets/imagenes-lugares/ParqueCentral4.jpg" height="300px" alt="Fourth slide">
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#lugar2" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#lugar2" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-      <div class="col-12 col-md-6 ">
-        <h2 class="text-center">Parque a Centroamérica</h2>
-        <p Align="justify">Consistía en realidad de 2 jardines, uno de los cuales era parte o extensión del atrio de la iglesia. Estaban separados por un portal cuya función era plenamente comercial. En 1938 se trazó su distribución final, durante el gobierno de Jorge Ubico. Los antiguos jardines fueron integrados en un solo cuerpo. Fue y sigue siendo centro de actividades importantes de caracter social y político, sufrió varias remodelaciones. El kiosko del Parque es un monumento a uno de los autores del Himno Nacional: Rafael Álfavez Ovalle.</p>
-      </div>
-    </div>
-    <br><br>
-    <div class="row">
-      <div class="col-12 col-md-6 ">
-        <h2 class="text-center">Cerro El Baúl</h2>
-        <p Align="justify">Constituye el pulmón urbano de la ciudad, el cual ha sido tradicionalmente un centro de recreación familiar. Puede subirse desde el departamento de Quetzaltenango ya sea a pie, en carro o bicicleta. Podrá observarse el casco urbano rodeado de pura naturaleza.
-        </p>
-      </div>
-      <div id="lugar3" class="carousel slide col-12 col-md-6" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active ">
-            <img class="d-block w-100" src="../assets/imagenes-lugares/ElBaul1.jpg" height="300px" alt="First slide">
-          </div>
-          <div class="carousel-item ">
-            <img class="d-block w-100" src="../assets/imagenes-lugares/ElBaul2.jpg" height="300px" alt="Second slide">
-          </div>
-          <div class="carousel-item ">
-            <img class="d-block w-100" src="../assets/imagenes-lugares/ElBaul3.jpg" height="300px" alt="Third slide">
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#lugar3" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#lugar3" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </div>
-    <br><br>
-    <div class="row">
-      <div id="lugar4" class="carousel slide col-12 col-md-6" data-ride="carousel">
-        <div class="carousel-inner">
-        <div class="carousel-item active ">
-        <img class="d-block w-100" src="../assets/imagenes-lugares/Teatro1.jpg" height="300px" alt="First slide">
-        </div>
-        <div class="carousel-item ">
-        <img class="d-block w-100" src="../assets/imagenes-lugares/Teatro2.jpg" height="300px" alt="Second slide">
-        </div>
-        <div class="carousel-item ">
-        <img class="d-block w-100" src="../assets/imagenes-lugares/Teatro3.jpg" height="300px" alt="Third slide">
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#lugar4" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#lugar4" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-      </div>
-      <div class="col-12 col-md-6 ">
-        <h2 class="text-center">Teatro Municipal</h2>
-        <p Align="justify">Este teatro pasó por varias restauraciones. Su diseño original es obra del arquitecto Durini, quien logra un estilo del neorrenacimiento francés debido a que hizo uso de elementos decorativos típicos de la cultura francesa. El entorno del teatro ha sido adornado con varios monumentos a lo largo del tiempo, de ellos el que más se destaca es la estatua de Artemisa, personaje de la mitología griega, diosa de la naturaleza, las cosechas y la caza.</p>
-      </div>
-    </div>
-    <br><br>
-    <div class="row">
-      <div class="col-12 col-md-6 ">
-        <h2 class="text-center">Catedral del Espíritu Santo</h2>
-        <p Align="justify">Se empezó a construir aproximadamente en mayo de 1532. Este templo fue fundado por los españoles luego de conquistar la Ciudad de Quetzaltenango y en honor al Espíritu Santo. Luego de sufrir deterioros por los terremotos de 1765, 1853 y otros años, la nueva catedral se construyó y el resultado fue la impresión de que hay dos iglesias unidas, pero en realidad es la fachada de la catedral antigua y la nueva construcción.</p>
-      </div>
-      <div id="lugar5" class="carousel slide col-12 col-md-6" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active ">
-          <img class="d-block w-100" src="../assets/imagenes-lugares/Catedral1.jpg" height="300px" alt="First slide">
-          </div>
-          <div class="carousel-item ">
-          <img class="d-block w-100" src="../assets/imagenes-lugares/Catedral2.jpg" height="300px" alt="Second slide">
-          </div>
-          <div class="carousel-item ">
-          <img class="d-block w-100" src="../assets/imagenes-lugares/Catedral3.jpg" height="300px" alt="Third slide">
-          </div>
-        </div>
-      <a class="carousel-control-prev" href="#lugar5" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#lugar5" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-      </div>
-    </div>
-    <hr>
+    <br><hr><br>
   </div>
 </template>
 
 <script>
-export default {}
+import JSONlugares from './lugares.json'
+
+export default {
+  data() {
+    return {
+      porPagina: 5,
+      paginaActual: 1,
+      lugares: JSONlugares
+    }
+  },
+  computed: {
+    rows() {
+      return 15
+    }
+  }
+}
 </script>
 
 <style>
