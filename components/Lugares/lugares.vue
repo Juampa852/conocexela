c19e54014d78887df2b8bb7bc93974fce93d9542
Mariano <marianorv97@gmail.com>
2019-07-09 12:11:22 -0600

    Paginación en Lugares Turísticos
    Se agregó la paginación para mostrar 5 lugares por página

--- components/Lugares/lugares.vue
+++ components/Lugares/lugares.vue
@@ -1,91 +1,95 @@
 <template>
-<body>
-  <div id="divlugares" class="container">
-    <div class="text-center">
-      <h1>Lugares Turísticos</h1>
-    </div>
-    <br />
-    <hr />
-    <br />
-    <!-- CICLO PARA MOSTRAR TODOS LOS LUGARES -->
-    <div v-for="(lugar, index) in lugares" :key="index">
-      <!-- ALINEADO A LA IZQUIERDA -->
-      <div v-if="index % 2 === 1">
-        <div class="row">
-          <!-- TEXTO -->
-          <div class="col-12 col-md-6">
-            <h2 class="text-center">{{ index }}. {{ lugar.nombre }}</h2>
-            <p align="justify">{{ lugar.descripcion }}</p>
-          </div>
-          <!-- SLIDER  -->
-          <div class="col-12 col-md-6">
-            <b-carousel
-              id="carousel-fade"
-              style="text-shadow: 0px 0px 2px #000"
-              fade
-              indicators
-              controls
-              img-width
-              img-height="300px"
-            >
-              <b-carousel-slide
-                v-for="imagen in lugar.imagenes"
-                :key="imagen"
-                :img-src="imagen"
-                class="imagen"
-              />
-            </b-carousel>
-          </div>
-        </div>
-        <br />
-        <br />
+  <body>
+    <div id="divlugares" class="container">
+      <div class="text-center">
+        <h1>Lugares Turísticos</h1>
       </div>
-      <!-- ALINEADO A LA DERECHA -->
-      <div v-else>
-        <div class="row">
-          <!-- SLIDER  -->
-          <div class="col-12 col-md-6">
-            <b-carousel
-              id="carousel-fade"
-              style="text-shadow: 0px 0px 2px #000"
-              fade
-              indicators
-              controls
-              img-width
-              img-height="300px"
-            >
-              <b-carousel-slide
-                v-for="imagen in lugar.imagenes"
-                :key="imagen"
-                :img-src="imagen"
-                class="imagen"
-              />
-            </b-carousel>
+      <br />
+      <hr />
+      <br />
+      <!-- CICLO PARA MOSTRAR TODOS LOS LUGARES -->
+      <div v-for="i in perPage" :key="i">
+        <!-- ALINEADO A LA IZQUIERDA -->
+        <div v-if="i % 2 === 1">
+          <div class="row">
+            <!-- TEXTO -->
+            <div class="col-12 col-md-6">
+              <h2 class="text-center">{{ ((numero-1)*perPage) + i }}. {{ lugares[(numero*perPage)-perPage-1+i].nombre }}</h2>
+              <p align="justify">{{ lugares[(numero*perPage)-perPage-1+i].descripcion }}</p>
+            </div>
+            <!-- SLIDER  -->
+            <div class="col-12 col-md-6">
+              <b-carousel
+                id="carousel-fade"
+                style="text-shadow: 0px 0px 2px #000"
+                fade
+                indicators
+                controls
+                img-width
+                img-height="300px"
+              >
+                <b-carousel-slide
+                  v-for="imagen in lugares[(numero*perPage)-perPage-1+i].imagenes"
+                  :key="imagen"
+                  :img-src="imagen"
+                  class="imagen"
+                />
+              </b-carousel>
+            </div>
           </div>
-          <!-- TEXTO -->
-          <div class="col-12 col-md-6">
-            <h2 class="text-center">{{ index }}. {{ lugar.nombre }}</h2>
-            <p align="justify">{{ lugar.descripcion }}</p>
-          </div>
+          <br />
+          <br />
         </div>
-        <br />
-        <br />
+        <!-- ALINEADO A LA DERECHA -->
+        <div v-else>
+          <div class="row">
+            <!-- SLIDER  -->
+            <div class="col-12 col-md-6">
+              <b-carousel
+                id="carousel-fade"
+                style="text-shadow: 0px 0px 2px #000"
+                fade
+                indicators
+                controls
+                img-width
+                img-height="300px"
+              >
+                <b-carousel-slide
+                  v-for="imagen in lugares[(numero*perPage)-perPage-1+i].imagenes"
+                  :key="imagen"
+                  :img-src="imagen"
+                  class="imagen"
+                />
+              </b-carousel>
+            </div>
+            <!-- TEXTO -->
+            <div class="col-12 col-md-6">
+              <h2 class="text-center">{{ ((numero-1)*perPage) + i }}. {{ lugares[(numero*perPage)-perPage-1+i].nombre }}</h2>
+              <p align="justify">{{ lugares[(numero*perPage)-perPage-1+i].descripcion }}</p>
+            </div>
+          </div>
+          <br />
+          <br />
+        </div>
       </div>
+      <br />
+      <hr />
+      <br />
     </div>
-    <br />
-    <hr />
-    <br />
-  </div>
-</body>
+  </body>
 </template>
 
 <script>
 import JSONlugares from './lugares.json'
 
 export default {
+  props: {
+    numero: Number
+  },
   data() {
     return {
-      lugares: JSONlugares
+      lugares: JSONlugares,
+      perPage: 5
     }
   }
 }
--- pages/lugares.vue
+++ pages/lugares.vue
@@ -1,20 +1,35 @@
 <template>
   <section>
     <Header />
-    <PaginaPrincipal />
+    <Lugares :numero="currentPage" />
+    <b-pagination
+      v-model="currentPage"
+      align="center"
+      :total-rows="rows"
+      :per-page="perPage"
+      prev-text="Anterior"
+      next-text="Siguiente"
+    ></b-pagination>
     <Footer />
   </section>
 </template>
 
 <script>
-import PaginaPrincipal from '~/components/Lugares/lugares.vue'
+import Lugares from '~/components/Lugares/lugares.vue'
 import Header from '~/assets/header'
 import Footer from '~/assets/footer.vue'
 export default {
   components: {
-    PaginaPrincipal,
+    Lugares,
     Header,
     Footer
+  },
+  data() {
+    return {
+      rows: 3,
+      perPage: 1,
+      currentPage: 1
+    }
   }
 }
 </script>
